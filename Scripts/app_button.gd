a165e215064ca2c353767e11cb4a681aedf0c45d
Max <supermax10x@gmail.com>
2026-03-23 22:34:43 -0400

    added stat page

--- Scripts/app_button.gd
+++ Scripts/app_button.gd
@@ -1,8 +1,11 @@
 extends TextureButton
-class_name Application
+class_name AppButton
 
 @export var app_name: String = ""
 @export var app_scene: PackedScene
+
+var app_node: Control
+var scene_added: bool = false
 
 func _ready() -> void:
 	pressed.connect(_on_button_pressed)
@@ -17,5 +20,13 @@ func _process(delta: float) -> void:
 		modulate = Color.WHITE
 
 func _on_button_pressed():
-	var app = app_scene.instantiate()
-	add_child(app, true)
+	if app_scene and not scene_added:
+		var app = app_scene.instantiate()
+		add_child(app, true)
+		app_node = app
+		app_node.global_position = (get_viewport_rect().size / 2 + app_node.size / 2)
+		scene_added = true
+		z_index = 100
+		
+	elif scene_added:
+		app_node.show()
